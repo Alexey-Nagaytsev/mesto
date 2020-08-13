6f10af9599ab1921f4c5734b0df2f7b2774f706a
Alexey Nagaytsev <kuchka2071@yandex.ru>
2020-08-13 21:19:17 +0300

    refactor: изменить структуру, удалить лишнее

--- scripts/index.js
+++ scripts/index.js
@@ -1,47 +1,42 @@
-const popup = document.querySelector('.popup');
-const openPopupButton = document.querySelector('.profile__edit-button');
-const popupCloseButton = popup.querySelector('.popup__close-button');
-const popupSaveButton = popup.querySelector('.popup__save-button');
-
+let popup = document.querySelector('.popup');
+let openPopupButton = document.querySelector('.profile__edit-button');
+let popupCloseButton = popup.querySelector('.popup__close-button');
+let popupSaveButton = popup.querySelector('.popup__save-button');
+// Находим форму
+let formElement = document.querySelector('.popup__form');
+// Находим поля формы в DOM
+let popupItemName = document.querySelector('.popup__item_name');
+let popupItemJob = document.querySelector('.popup__item_job');
+// Выбираем элементы, куда должны быть вставлены значения полей
+let profileTitle = document.querySelector('.profile__title');
+let profileSubtitle  = document.querySelector('.profile__subtitle');
 
 //открытие окна
 const popupOpened = function() {
   popup.classList.add('popup_opened');
+  profileTitle.value = nameInput.textContent;
+  profileSubtitle.value = jobInput.textContent;
 }
-openPopupButton.addEventListener('click', popupOpened);
 
 //закрытие окна
 const popupClosed = function() {
   popup.classList.remove('popup_opened');
 }
-popupCloseButton.addEventListener('click', popupClosed);
 
 //редактирование данных
-let formElement = document.querySelector('.popup__form');
 function formSubmitHandler (evt) {
   evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы. // Так мы можем определить свою логику отправки.
-
-  // Находим поля формы в DOM
-  let popupItemName = document.querySelector('.popup__item_name');
-  let popupItemJob = document.querySelector('.popup__item_job');
-
   // Получите значение полей из свойства value
   let nameInput = popupItemName.value;
   let jobInput = popupItemJob.value;
-
-  // Выберftv элементы, куда должны быть вставлены значения полей
-  let profileTitle = document.querySelector('.profile__title');
-  let profileSubtitle  = document.querySelector('.profile__subtitle');
-
-  // новые значения
+  // новые значения // сохранения данных
   profileTitle.textContent = nameInput;
   profileSubtitle.textContent = jobInput;
+  popupClosed();
 }
 
+// Слушатели Открытия/Закрытия/Сохранения
+openPopupButton.addEventListener('click', popupOpened);
+popupCloseButton.addEventListener('click', popupClosed);
 formElement.addEventListener('submit', formSubmitHandler);
 
-// сохранения данных
-function saveValues() {
-  popup.classList.remove('popup_opened');
-}
-popupSaveButton.addEventListener('click', saveValues);
